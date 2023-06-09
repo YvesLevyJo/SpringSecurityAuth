8ebfcf6fe694fbfd9c3bcbd8e2040aa2f8740212
Yves MBULU NTOTO <YMB23612@open-groupe.com>
2023-06-09 17:01:43 +0200

    install DONE

--- src/main/java/com/openclassrooms/login/configuration/SpringSecurityConfig.java
+++ src/main/java/com/openclassrooms/login/configuration/SpringSecurityConfig.java
@@ -1,5 +1,59 @@
 package com.openclassrooms.login.configuration;
 
-public class SpringSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig{
+	
+	/**
+	 * Configure données de test
+	 * @param auth
+	 * @throws Exception
+	 */
+	@Bean
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("springuser").password(passwordEncoder().encode("spring123")).roles("USER")
+		.and()
+		.withUser("springadmin").password(passwordEncoder().encode("admin123")).roles("ADMIN","USER");
+	}
+	
+	/***
+	 * Chaîne de filtrage des reqûetesq HTTP,
+	 * verrouillant l'accès à certaines page en fonction de différents rôles
+	 * @param http
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+		 .authorizeHttpRequests((requests) -> requests
+				 .requestMatchers("/", "/home").permitAll()
+				 .anyRequest().authenticated()
+				 )
+		 .formLogin((form) -> form
+		 	.loginPage("/login")
+		 	.permitAll()
+		 )
+		 .logout((logout) -> logout.permitAll());
+		
+		return http.build();
+	}
+	
+	/***
+	 * Hashage du mot de passe
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
