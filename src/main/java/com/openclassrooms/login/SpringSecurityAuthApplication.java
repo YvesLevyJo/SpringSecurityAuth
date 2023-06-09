98a7219f29cbb30c07a5a43316e9be2b40f88b29
Yves MBULU NTOTO <YMB23612@open-groupe.com>
2023-06-09 15:36:52 +0200

    change package name in lowercase

--- src/main/java/com/openclassrooms/login/SpringSecurityAuthApplication.java
+++ src/main/java/com/openclassrooms/login/SpringSecurityAuthApplication.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.SpringSecurityAuth;
+package com.openclassrooms.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
